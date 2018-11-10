6b86557250bc50f7bf100abcffc4f9bf92aba021
MacPro <antonny@MacBook-Pro-de-MacPro.local>
2018-11-10 21:49:11 +0100

    Update Training4/WorkSpace/RESTService_Test/src/test/java/com/sever/demo/RestServiceTestApplicationTests.java

--- Training4/WorkSpace/RESTService_Test/src/test/java/com/sever/demo/RestServiceTestApplicationTests.java
+++ Training4/WorkSpace/RESTService_Test/src/test/java/com/sever/demo/RestServiceTestApplicationTests.java
@@ -6,12 +6,14 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.validator.internal.util.privilegedactions.NewProxyInstance;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,6 +21,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.sever.demo.controller.ProductController;
 import com.sever.demo.model.ProductModel;
 import com.sever.demo.repository.ProductRepository;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -36,15 +42,18 @@ public class RestServiceTestApplicationTests {
 		product1 = new ProductModel("A12","Coca-cola","bebida",1);
 		product2 = new ProductModel("B12","Pepsi","bebida",2);
 		
+//		if(productRepository.findByCode("A12") == null) {
+//			productRepository.save(product1);
+//		} else if(productRepository.findByCode("B12") == null){
+//			productRepository.save(product2);
+//		}
+		
 		productController = new ProductController(productRepository);
-		
-		
-		
 	}
 	
 	
 	@Test
-	public void listProducts_OK() {
+	public void listAllProducts_OK() {
 		List<ProductModel> listRespuesta = new ArrayList<>();
 		listRespuesta.add(product1);
 		listRespuesta.add(product2);
@@ -60,7 +69,41 @@ public class RestServiceTestApplicationTests {
 	}
 	
 	@Test
-	public void showProduct_OK() {
+	public void addProduct_OK() {
+
+		ProductModel nuevoProducto = new ProductModel("C123","platano","1kg de fruta",2);
+		UriComponentsBuilder builderEsperado = UriComponentsBuilder.fromUriString("http://localhost:8080");
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("http://localhost:8080");
+		
+		HttpHeaders header = new HttpHeaders();
+		header.setLocation(builderEsperado.path("/resume/{code}").buildAndExpand(nuevoProducto.getCode()).toUri());
+		ResponseEntity<?> esperado = new ResponseEntity<String>(header, HttpStatus.CREATED);
+		
+		when(productRepository.save(nuevoProducto)).thenReturn(nuevoProducto);
+		
+		ResponseEntity<?> resultado = productController.addProduct(nuevoProducto,builder);
+		Assert.assertEquals(esperado, resultado);
+		
+		verify(productRepository,times(1)).save(nuevoProducto);
+		
+	}
+	
+	
+	@Test
+	public void deleteProduct_OK() {
+
+		ResponseEntity<?> esperado = new ResponseEntity<ProductModel>(HttpStatus.NO_CONTENT);
+		when(productRepository.deleteByCode(product1.getCode())).thenReturn(product1);
+		
+		ResponseEntity<?> resultado = productController.deleteProduct("A12");
+		Assert.assertEquals(esperado, resultado);
+		
+		verify(productRepository,times(1)).deleteByCode(product1.getCode());
+		
+	}
+	
+	@Test
+	public void getProduct_OK() {
 		
 		ResponseEntity<?> esperado = new ResponseEntity<ProductModel>(product1, HttpStatus.OK);
 		when(productRepository.findByCode("A12")).thenReturn(product1);
@@ -69,7 +112,6 @@ public class RestServiceTestApplicationTests {
 		Assert.assertEquals(esperado, resultado);
 		
 		verify(productRepository,times(1)).findByCode("A12");
-		
 	}
 	
 	
